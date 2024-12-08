63a02610e1fd58892d55ca70d1d4ec073a135257
M-Kashif-ALI <158289645+M-Kashif-ALI@users.noreply.github.com>
2024-12-08 23:30:30 +0500

    Completed

---
+++ src/app/page.tsx
@@ -0,0 +1,23 @@
+import Banner1 from "./components/banner1";
+import Banner2 from "./components/banner2";
+import Banner3 from "./components/banner3";
+import EditorsPick from "./components/editorsPick";
+import Featured from "./components/featured";
+import FeaturedPost from "./components/featuredPost";
+import Footer from "./components/footer";
+import NavBar from "./components/navBar";
+
+export default function Home() {
+  return (
+    <main>
+      <NavBar />
+      <Banner1 />
+      <EditorsPick />
+      <Featured />
+      <Banner2 />
+      <Banner3 />
+      <FeaturedPost />
+      <Footer />
+    </main>
+  );
+}
